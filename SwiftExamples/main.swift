3975c2c9afbaf1a0132c188e2c2037be8a340848
JHorace <jasumihiro@gmail.com>
2021-03-11 17:30:20 -0800

    Added a TwoSum implementation using binary search. This has not been tested yet.

--- SwiftExamples/main.swift
+++ SwiftExamples/main.swift
@@ -6,6 +6,45 @@
 //
 
 import Foundation
+import SwiftUtils
 
-print("Hello, World!")
+
+//  Finds each pair of integers in an array whose sum is equal to target
+func twoSum<T: SignedInteger>(_ nums: [T], _ target: T) -> [[T]]?
+{
+    var result: [[T]] = [];
+    
+    //  Copy and sort the array to allow binary search
+    var sorted_nums = nums;
+    sorted_nums.sort();
+    
+    //  Keep track of previously used numbers to avoid duplicates
+    var num_set = Set<T>();
+    
+    for (index, num) in sorted_nums.enumerated()
+    {
+        if(!num_set.contains(num))
+        {
+            num_set.insert(num);
+            
+            let compliment = binarySearch(nums.suffix(nums.endIndex - index),
+                                          target - num);
+            
+            if(compliment != nil)
+            {
+                result.append([num, target - num]);
+            }
+        }
+    }
+    
+    if(result.isEmpty)
+    {
+        return nil;
+    }
+    else
+    {
+        return result;
+    }
+
+}
 
